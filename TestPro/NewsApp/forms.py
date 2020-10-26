26f51841856a2903519bf7c65a8f66ca5028907d
milanovivcb <milanovicful@gmail.com>
2020-10-26 09:35:52 +0100

    Added forms.py, models.py, signup.html in order to create a signup page where csrf token is in and afterwards creating in the forms.py the widgets that I will use as custom tags in the signup.html to achieve the very same thing. Practice 2

--- TestPro/NewsApp/forms.py
+++ TestPro/NewsApp/forms.py
@@ -2,7 +2,20 @@ from django import forms
 
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=100)
-    password = forms.CharField(max_length=100)
-    email = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=100)
+    username = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'Username',
+                               })),
+    password = forms.CharField(max_length=100,
+                               widget=forms.TextInput(attrs={
+                                   'class': 'form-control'
+                               })),
+    email = forms.CharField(max_length=100,
+                            widget=forms.TextInput(attrs={
+                                'class': 'form-control'
+                            })),
+    phone = forms.CharField(max_length=100,
+                            widget=forms.TextInput(attrs={
+                                'class': 'form-control'
+                            }))
